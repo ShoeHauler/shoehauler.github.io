8187be89eab6a761d351401481e8799c6538df4f
Shadowsniper784 <143124585+ShoeHauler@users.noreply.github.com>
2023-09-05 18:14:13 +0100

    Reviews, soldOutSizes, easier images

--- product.js
+++ product.js
@@ -5,6 +5,7 @@ function changeImage(imageUrl) {
 
 // JavaScript to handle size option selection
 function selectSize(element) {
+    if(element.getAttribute("disabled")) return;
     const sizeOptions = document.querySelectorAll('.size-option');
 
     // Deselect all size options
@@ -20,7 +21,7 @@ function selectSize(element) {
 function selectSize(element) {
     const sizeOptions = document.querySelectorAll('.size-option');
     const sellingFastMessage = document.getElementById('selling-fast-message');
-    
+
     // Deselect all size options
     sizeOptions.forEach((option) => {
         option.classList.remove('selected');
@@ -49,11 +50,11 @@ function disableSoldOutSizes() {
 
     sizeOptions.forEach((option) => {
         const size = parseFloat(option.getAttribute('data-size'));
-        
-        if (soldOutSizes.includes(size)) {
-            option.classList.add('sold-out'); // Apply the "sold-out" class
-            option.setAttribute('disabled', true); // Disable the option
-        }
+
+        // if (soldOutSizes.includes(size)) {
+        //     option.classList.add('sold-out'); // Apply the "sold-out" class
+        //     option.setAttribute('disabled', true); // Disable the option
+        // }
     });
 }
 
@@ -84,14 +85,92 @@ let productId = new URLSearchParams(location.search).get("productId")
 loadProductData(productId)
 
 // Function to update the number of reviews
-// function updateNumberOfReviews(newCount) {
-//     numberOfReviews = newCount;
-//     const reviewsButtonText = document.querySelector('.reviews-button-text');
-//     reviewsButtonText.innerHTML = `Reviews (${numberOfReviews})`;
-// }
+function updateNumberOfReviews(newCount) {
+    numberOfReviews = newCount;
+    const reviewsButtonText = document.querySelector('.reviews-button-text');
+    reviewsButtonText.innerHTML = `Reviews (${numberOfReviews})`;
+}
 
 // Example usage: To update the number of reviews to 3
 // updateNumberOfReviews(2);
+
+function getImageURL(baseDir, imageNumber) {
+    return "Images/ProductImages/" + baseDir + "Image" + imageNumber + ".jpg"
+}
+
+function createReviewElement(review) {
+    let username = review.username
+    let rating = review.rating
+    let reviewText = review.comment
+    let pfp = review.pfp
+    let date = review.date
+
+    let reviewElement = document.createElement("div")
+    reviewElement.classList.add("review")
+
+    let ratingContainer = document.createElement("div")
+    ratingContainer.classList.add("rating")
+
+    let stars = document.createElement("div")
+    stars.classList.add("stars")
+
+    //Font awsome stars, also support half stars
+    for (let i = 0; i < 5; i++) {
+        let star = document.createElement("span")
+        star.classList.add("fa")
+        if (i < rating) {
+            star.classList.add("fa-star")
+            star.classList.add("checked")
+        } else {
+            star.classList.add("fa-star")
+        }
+        stars.appendChild(star)
+    }
+
+    let plainRating = document.createElement("div")
+    plainRating.classList.add("plain-rating")
+    plainRating.textContent = rating + " Stars"
+
+    ratingContainer.appendChild(stars)
+
+    ratingContainer.appendChild(plainRating)
+
+    reviewElement.appendChild(ratingContainer)
+
+    let reviewTextElement = document.createElement("p")
+    reviewElement.classList.add("review-text")
+    reviewTextElement.textContent = reviewText
+    reviewElement.appendChild(reviewTextElement)
+
+    let userInfo = document.createElement("div")
+    userInfo.classList.add("user-info")
+
+    let pfpElement = document.createElement("img")
+    pfpElement.alt = "Profile picture of " + username
+    pfpElement.src = pfp
+
+    userInfo.appendChild(pfpElement)
+
+    let usernameElement = document.createElement("p")
+    usernameElement.classList.add("username")
+    usernameElement.textContent = username
+
+    let dateElement = document.createElement("p")
+    dateElement.classList.add("date")
+    dateElement.textContent = new Date(date).toLocaleDateString()
+
+    let div = document.createElement("div")
+    div.classList.add("username-date")
+
+    div.appendChild(usernameElement)
+    div.appendChild(dateElement)
+
+    userInfo.appendChild(div)
+
+    reviewElement.appendChild(userInfo)
+
+    return reviewElement
+}
 
 // Function to fetch and display product data
 function loadProductData(productId) {
@@ -110,25 +189,44 @@ function loadProductData(productId) {
                 const productPrice = document.getElementById("product-price");
                 const productImages = document.querySelector(".image-selection-list>ul");
                 const mainImage = document.getElementById("product-image")
+                const sizeOptions = document.querySelectorAll('.size-option');
+                const reviews = document.getElementById('reviews');
+                let reviewsButtonText = document.querySelector('.reviews-button-text');
+
+                let sizesNotAvailable = product.sizesNotAvailable
+                sizeOptions.forEach((option) => {
+                    if (sizesNotAvailable.includes(parseFloat(option.getAttribute('data-size'), 10))) {
+                        option.remove()
+                    }
+                })
+
+                // Update the number of reviews
+                updateNumberOfReviews(product.reviews.length);
+
+                // Display the reviews
+                product.reviews.forEach((review) => {
+                    const reviewElement = createReviewElement(review);
+                    reviews.appendChild(reviewElement);
+                });
+
+
 
                 productName.textContent = product.name;
                 productDescription.textContent = product.description;
                 productPrice.textContent = product.price;
-                mainImage.src = product.images[0]
-                console.log(productImages)
-                product.images.forEach(x=>{
-                    console.log(x)
+                mainImage.src = getImageURL(product.baseDir, 1)
+                new Array(product.imageCount).fill(0).forEach((_, i) => {
                     let li = document.createElement("li")
                     let img = document.createElement("img")
-                    img.src  = x;
+                    img.src = getImageURL(product.baseDir, i + 1)
                     img.alt = "An image icon"
-                    img.addEventListener('click', function(){
-                        changeImage(x)
+                    img.addEventListener('click', function () {
+                        changeImage(img.src)
                     });
                     li.appendChild(img)
                     productImages.appendChild(li)
                 })
-                
+
             } else {
                 // Handle the case where the product is not found
                 console.log('Product not found');
