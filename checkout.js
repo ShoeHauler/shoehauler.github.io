44a6f7090751bfd27826ff131fca8422d1231ab8
Razorclaw133 <shoehauler@gmail.com>
2023-09-16 19:18:18 +0100

    First part of checkout system

--- checkout.js
+++ checkout.js
@@ -1,3 +1,3 @@
 let productId = new URLSearchParams(location.search).get("productId")
 
-document.getElementById("code").innerText = productId
+document.getElementById("code").innerText = productId   
